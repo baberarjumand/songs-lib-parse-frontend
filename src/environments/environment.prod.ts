1f410ce063ca29c7b6af71d84f375f83626035e2
Baber Arjumand <baber.arjumand@gmail.com>
2021-01-18 15:10:38 +0500

    set env file vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  parseAppId: 'APP_ID',
+  parseMasterKey: 'MASTER_KEY',
+  mongoDbUri: 'MONGODB_URI',
+  parseServerUrl: 'SERVER_URL',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  parseAppId: 'APP_ID',
+  parseMasterKey: 'MASTER_KEY',
+  mongoDbUri: 'MONGODB_URI',
+  parseServerUrl: 'SERVER_URL',
 };
 
 /*
