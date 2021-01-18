a6a5120baea0db7075857d93dba12c0273070bae
Baber Arjumand <baber.arjumand@gmail.com>
2021-01-18 16:52:22 +0500

    implemented home page with all func in service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,69 @@
+import { ParseDbService } from './../../services/parse-db.service';
 import { Component, OnInit } from '@angular/core';
+import { Song } from 'src/app/model/song.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  isLoading = true;
 
-  constructor() { }
+  resultSet: Song[];
+
+  constructor(private dbService: ParseDbService) {}
 
   ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.isLoading = false;
+    // }, 1500);
+
+    this.updateResultSet();
   }
 
+  async updateResultSet(): Promise<void> {
+    this.isLoading = true;
+
+    this.resultSet = await this.dbService.getAllSongs();
+    console.log('Response:', this.resultSet);
+
+    this.isLoading = false;
+  }
+
+  async createSong(): Promise<void> {
+    this.isLoading = true;
+
+    try {
+      const response = await this.dbService.createSong();
+      console.log('New record created:', response);
+
+      await this.updateResultSet();
+
+      alert('New Record Created:\n' + JSON.stringify(response, null, 2));
+    } catch (error) {
+      // error handling logic
+      alert(JSON.stringify(error, null, 2));
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  async deleteSong(): Promise<void> {
+    this.isLoading = true;
+
+    try {
+      const response = await this.dbService.deleteSong();
+      console.log('Record deleted:', response);
+
+      await this.updateResultSet();
+
+      alert('Record deleted:\n' + JSON.stringify(response, null, 2));
+    } catch (error) {
+      // error handling logic
+      alert(JSON.stringify(error, null, 2));
+    } finally {
+      this.isLoading = false;
+    }
+  }
 }
--- src/app/services/parse-db.service.ts
+++ src/app/services/parse-db.service.ts
@@ -1,9 +1,56 @@
+import { Song } from './../model/song.model';
 import { Injectable } from '@angular/core';
 
+import * as Parse from 'parse';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ParseDbService {
+  constructor() {}
 
-  constructor() { }
+  async getAllSongs(): Promise<Song[]> {
+    try {
+      const response = await Parse.Cloud.run('getAllSongs');
+      const mappedResponse = this.mapResponse(response);
+      return mappedResponse;
+    } catch (error) {
+      alert(JSON.stringify(error, null, 2));
+    }
+  }
+
+  mapResponse(responseSet): Song[] {
+    const tempArr = [];
+
+    responseSet.forEach((element) => {
+      const tempObj: Song = {
+        id: element.id,
+        name: element.attributes.name,
+        artist: element.attributes.artist,
+        album: element.attributes.album,
+      };
+
+      tempArr.push(tempObj);
+    });
+
+    return tempArr;
+  }
+
+  async createSong(): Promise<void> {
+    try {
+      const response = await Parse.Cloud.run('createSong');
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteSong(): Promise<void> {
+    try {
+      const response = await Parse.Cloud.run('deleteSong');
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
